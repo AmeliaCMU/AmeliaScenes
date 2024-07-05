6b4e9fd16e11a9f5fcf68e187c9e135acc21b1ec
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-07-05 12:13:09 -0400

    (setup.py) Added missing libraries

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from gettext import install
 from setuptools import setup, find_packages
 
 setup(
@@ -5,4 +6,18 @@ setup(
     packages=find_packages(['./tools/*'], exclude=['test*']),
     version='1.0',
     description='Tool for getting interaction scores of a given scene',
+    install_requires=[
+        'easydict==1.10',
+        'joblib==1.2.0',
+        'networkx==3.1',
+        'numpy==1.21.2,<2',
+        'opencv-python==4.7.0.72,<4.8',
+        'pandas==2.0.3,<3',
+        'python-dateutil==2.9.0.post0',
+        'pytz==2024.1',
+        'shapely==2.0.3,<3',
+        'six==1.16.0,<2',
+        'tqdm==4.65.0,<5',
+        'tzdata==2024.1',
+    ]
 )
