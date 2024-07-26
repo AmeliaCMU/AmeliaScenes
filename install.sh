a147a4bf3d1f19c1494fa841a4fd138243912065
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-07-26 16:27:09 -0400

    Install amelia framework

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
-# git clone git@github.com:AmeliaCMU/AmeliaScenes.git
-# cd AmeliaScenes
+#!/bin/bash
+
 # conda create --name $1 python=3.9
 # conda activate $1
-# pip install -e .
+
+
+mkdir amelia
+cd amelia
+
+# array of arguments
+repos=(git@github.com:AmeliaCMU/AmeliaScenes.git git@github.com:AmeliaCMU/AmeliaMaps.git git@github.com:AmeliaCMU/AmeliaTF.git git@github.com:AmeliaCMU/AmeliaInference.git)
+amelia_framework=(AmeliaScenes AmeliaMaps AmeliaTF AmeliaInference)
+
+# loop through the array
+for i in "${!repos[@]}"; do
+    echo "Cloning ${repos[$i]}"
+    git clone ${repos[$i]}
+    pip install -e ${amelia_framework[$i]}
+done
